1b5e7c17a81d848b5382f93276bb153116ea05a9
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-07-06 12:26:53 +0200

    Add solution to challenge 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn info<T: std::fmt::Debug + std::fmt::Display>(text: &T) {
+    println!("Display formatting output: {}", text);
+    println!("Debug formatting output:  {:?}", text);
+    println!("Pretty-printed, debug formatting output: {:#?}", text);
+
+    println!(); // just for a line break
+}
+
 fn main() {
-    println!("Hello, Rust learner!");
+    let a = "&str";
+    let b = "string".to_string();
+
+    info(&a);
+    info(&b);
 }
