74141f06f8ad0871366c7017d16550f227df383a
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-07-07 17:08:41 +0200

    Solution to challenge 7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+extern crate chrono;
+
+trait Deadline {
+    fn is_passed(&self) -> bool;
+}
+
+use chrono::prelude::*;
+
+#[derive(Debug)]
+struct ImportantEvent {
+    name: String,
+    date: DateTime<Utc>,
+}
+
+impl Deadline for ImportantEvent {
+    fn is_passed(&self) -> bool {
+        return self.date < Utc::now();
+    }
+}
+
 fn main() {
-    println!("Hello, Rust learner!");
+    let event = ImportantEvent {
+        name: String::from("Time now"),
+        date: Utc::now(),
+    };
+
+    println!("{:#?}", event);
+}
+
+// Tests are inspired from the course
+#[test]
+fn in_past() {
+    use chrono::Duration;
+
+    let event = ImportantEvent {
+        name: String::from("friend's birthday"),
+        date: (Utc::now() - Duration::hours(25)).into(),
+    };
+
+    assert!(event.is_passed())
+}
+
+#[test]
+fn in_future() {
+    use chrono::Duration;
+
+    let event = ImportantEvent {
+        name: String::from("friend's birthday"),
+        date: (Utc::now() + Duration::hours(25)).into(),
+    };
+
+    println!("{:#?}", Local::now());
+
+    assert!(!event.is_passed())
 }
