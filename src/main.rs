66bd4479bc0da39781556266895291294ef05360
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-07-15 22:24:34 +0200

    Solution to challenge 10

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate chrono;
+
+use chrono::NaiveDate;
+use std::convert::TryInto;
+
+fn weeks_between(a: &str, b: &str) -> i32 {
+    let start_date = NaiveDate::parse_from_str(a, "%Y-%m-%d").unwrap();
+    let end_date = NaiveDate::parse_from_str(b, "%Y-%m-%d").unwrap();
+
+    return end_date
+        .signed_duration_since(start_date)
+        .num_weeks()
+        .try_into()
+        .unwrap();
+}
+
 fn main() {
-    println!("Hello, Rust learner!");
+    let n_weeks = weeks_between("2010-01-21", "2010-10-21");
+
+    println!("hello: {}", n_weeks);
+}
+
+// tests are from the course
+#[test]
+fn same_day() {
+    let n_weeks = weeks_between("1010-10-10", "1010-10-10");
+    assert_eq!(n_weeks, 0);
+}
+
+#[test]
+fn one_week() {
+    let n_weeks = weeks_between("1010-10-10", "1010-10-18");
+    assert_eq!(n_weeks, 1);
+}
+
+#[test]
+fn past() {
+    let n_weeks = weeks_between("1010-10-18", "1010-10-10");
+    assert_eq!(n_weeks, -1);
 }
